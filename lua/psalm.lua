f3c56ef4bb81641f5c04788cb6a2bca6a6d48118
Thiago Pontes <github@thiago.me>
2021-07-25 11:35:08 -0400

    get it working

--- lua/psalm.lua
+++ lua/psalm.lua
@@ -1,15 +1,30 @@
-local lspconfig = require("lspconfig")
-local configs = require("lspconfig/configs")
+local configs = require "lspconfig/configs"
+local util = require "lspconfig/util"
 
-if not lspconfig.psalm then
-  configs.psalm = {
+local server_name = "psalm"
+local bin_name = "psalm-language-server"
+
+configs[server_name] = {
+  default_config = {
+    cmd = {bin_name},
+    filetypes = {"php"},
+    root_dir = function(pattern)
+      local cwd = vim.loop.cwd()
+      local root = util.root_pattern("composer.json", ".git")(pattern)
+
+      -- prefer cwd if root is a descendant
+      return util.path.is_descendant(cwd, root) and cwd or root
+    end
+  },
+  docs = {
+    description = [[
+ https://github.com/vimeo/psalm/
+
+ Installation: https://psalm.dev/docs/running_psalm/installation/
+ ]],
     default_config = {
-      cmd = "psalm",
-      filetypes = {"php"},
-      root_dir = function(fname)
-        return lspconfig.util.find_git_ancestor(fname)
-      end,
-      settings = {}
+      cmd = {"psalm-language-server"},
+      root_dir = [[root_pattern("composer.json", ".git")]]
     }
   }
-end
+}
